43c3179824eebcb5758c514112b721d621420324
zongronghuang <zongrong.h886@gmail.com>
2020-09-10 15:37:59 +0800

    feat: create Navbar.vue page

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,22 @@
 <template>
   <div id="restaurant-forum">
-    <router-view />
+    <Navbar />
+    <main role="main" class="mt-5 bg-white">
+      <router-view />
+    </main>
   </div>
 </template>
 
 <script>
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "../src/components/Navbar.vue";
+
 export default {
   name: "App",
+  components: {
+    Navbar,
+  },
 };
 </script>
 
