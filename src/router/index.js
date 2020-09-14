0925b4458373da8ae56db350086ad50f016ff777
zongronghuang <zongrong.h886@gmail.com>
2020-09-14 17:21:01 +0800

    feat: create Restaurant page

--- src/router/index.js
+++ src/router/index.js
@@ -39,6 +39,11 @@ const routes = [
     component: () => import('../views/RestaurantsTop.vue')
   },
   {
+    path: '/restaurants/:id',
+    name: 'restaurant',
+    component: () => import('../views/Restaurant.vue')
+  },
+  {
     path: '/users/top',
     name: 'users-top',
     component: () => import('../views/UsersTop.vue')
