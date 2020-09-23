d2d41e465be05aa0c94266a82292e211ee8a95c7
zongronghuang <zongrong.h886@gmail.com>
2020-09-23 17:18:48 +0800

    feat: complete editing functions for AdminCategories page

--- src/views/AdminCategories.vue
+++ src/views/AdminCategories.vue
@@ -25,10 +25,28 @@
         <tr v-for="category in categories" :key="category.id">
           <th scope="row">{{ category.id }}</th>
           <td class="position-relative">
-            <div class="category-name">{{ category.name }}</div>
+            <div class="category-name" v-show="!category.isEditing">{{ category.name }}</div>
+            <input
+              v-show="category.isEditing"
+              v-model="category.name"
+              type="text"
+              class="form-control"
+            />
+            <span v-show="category.isEditing" class="cancel" @click="handleCancel(category.id)">✕</span>
           </td>
           <td class="d-flex justify-content-between">
-            <button type="button" class="btn btn-link mr-2">Edit</button>
+            <button
+              type="button"
+              class="btn btn-link mr-2"
+              v-show="!category.isEditing"
+              @click.stop.prevent="toggleIsEditing(category.id)"
+            >Edit</button>
+            <button
+              v-show="category.isEditing"
+              type="button"
+              class="btn btn-link mr-2"
+              @click.prevent.stop="updateCategory({categoryId: category.id, name: category.name})"
+            >Save</button>
             <button
               type="button"
               class="btn btn-link mr-2"
@@ -90,7 +108,11 @@ export default {
   },
   methods: {
     fetchCategories() {
-      this.categories = dummyData.categories;
+      this.categories = dummyData.categories.map((category) => ({
+        ...category,
+        isEditing: false,
+        nameCached: "",
+      }));
     },
     createCategory() {
       this.categories.push({
@@ -105,6 +127,66 @@ export default {
         (category) => category.id !== categoryId
       );
     },
+    toggleIsEditing(categoryId) {
+      this.categories = this.categories.map((category) => {
+        if (category.id === categoryId) {
+          return {
+            ...category,
+            isEditing: !category.isEditing,
+            nameCached: category.name,
+          };
+        }
+
+        return category;
+      });
+    },
+    updateCategory({ categoryId }) {
+      this.toggleIsEditing(categoryId);
+    },
+    handleCancel(categoryId) {
+      this.categories = this.categories.map((category) => {
+        if (category.id === categoryId) {
+          return {
+            ...category,
+            name: category.nameCached,
+          };
+        }
+
+        return category;
+      });
+
+      this.toggleIsEditing(categoryId);
+    },
   },
 };
 </script>
+
+<style scoped>
+.category-name {
+  padding: 0.375rem 0.75rem;
+  border: 1px solid transparent;
+  outline: 0;
+  cursor: auto;
+}
+
+.btn-link {
+  width: 62px;
+}
+
+.cancel {
+  position: absolute;
+  right: 20px;
+  top: 50%;
+  transform: translateY(-50%);
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  width: 25px;
+  height: 25px;
+  border: 1px solid #aaaaaa;
+  border-radius: 50%;
+  user-select: none;
+  cursor: pointer;
+  font-size: 12px;
+}
+</style>
