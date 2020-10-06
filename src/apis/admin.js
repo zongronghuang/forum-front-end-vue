839454f68c4a16315ef52d37dcb321fda73b184c
Zong-Rong Huang <zongrong.h886@gmail.com>
2020-10-07 00:28:41 +0800

    refactor: remove getDetail API from admin.js

--- src/apis/admin.js
+++ src/apis/admin.js
@@ -13,11 +13,6 @@ export default {
         headers: { Authorization: `Bearer ${getToken()}` }
       })
     },
-    getDetail({ categoryId }) {
-      return apiHelper.get(`/admin/categories/${categoryId}`, {
-        headeres: { Authorization: `Bearer ${getToken()}` }
-      })
-    },
     update({ categoryId, name }) {
       return apiHelper.put(`/admin/categories/${categoryId}`, { name }, {
         headers: { Authorization: `Bearer ${getToken()}` }
