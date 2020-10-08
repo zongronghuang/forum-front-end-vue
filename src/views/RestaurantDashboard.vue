137cee0c3e91df18de67172e611d2d4e482e4bc7
Zong-Rong Huang <zongrong.h886@gmail.com>
2020-10-08 23:50:30 +0800

    feat: add restaurantsAPI.getRestaurant() to RestaurantDashboard.vue

--- src/views/RestaurantDashboard.vue
+++ src/views/RestaurantDashboard.vue
@@ -2,7 +2,9 @@
   <div class="container py-5">
     <div>
       <h1>{{ restaurant.name }}</h1>
-      <span class="badge badge-secondary mt-1 mb-3">{{ restaurant.Category.name }}</span>
+      <span class="badge badge-secondary mt-1 mb-3">{{
+        restaurant.Category.name
+      }}</span>
     </div>
 
     <hr />
@@ -12,94 +14,15 @@
       <li>瀏覽次數： {{ restaurant.viewCounts }}</li>
     </ul>
 
-    <button type="button" class="btn btn-link" @click="$router.back()">回上一頁</button>
+    <button type="button" class="btn btn-link" @click="$router.back()">
+      回上一頁
+    </button>
   </div>
 </template>
 
 <script>
-const dummyData = {
-  restaurant: {
-    id: 10,
-    name: "yoyoyoy ",
-    tel: "(506) 198-3977",
-    address: "4747 Lubowitz Brook",
-    opening_hours: "08:00",
-    description:
-      "Aut quia aut ea nostrum quae doloremque necessitatibus. Eos illo quia et. Culpa repellat eos minus necessitatibus aut debitis accusamus.\r\n \r\nAssumenda aut itaque corrupti soluta et aut est. Hic non dignissimos odit qui. Optio doloribus rerum molestiae dolorem commodi. Culpa et officiis.\r\n \r\nCulpa laborum ea eos praesentium enim quasi eos. Nobis praesentium maxime soluta delectus eveniet reiciendis aut. Voluptates nesciunt et explicabo.",
-    image:
-      "https://loremflickr.com/320/240/restaurant,food/?random=6.84802493817025",
-    viewCounts: 69,
-    createdAt: "2020-02-28T14:38:32.000Z",
-    updatedAt: "2020-09-04T08:04:08.000Z",
-    CategoryId: 5,
-    Category: {
-      id: 5,
-      name: "素食料理",
-      createdAt: "2020-02-28T14:38:32.000Z",
-      updatedAt: "2020-02-28T14:38:32.000Z",
-    },
-    Comments: [
-      {
-        id: 10,
-        text:
-          "Nulla accusamus exercitationem dolor dolorum mollitia velit corporis.",
-        UserId: 2,
-        RestaurantId: 10,
-        createdAt: "2020-02-28T14:38:32.000Z",
-        updatedAt: "2020-02-28T14:38:32.000Z",
-        User: {
-          id: 2,
-          name: "user1",
-          email: "user1@example.com",
-          password:
-            "$2a$10$NyaAtgRuHx3i7hHlnb5IXOC4Uk4.q1J1iQs3op.ymdCEh7.tOwcH2",
-          isAdmin: false,
-          image: null,
-          createdAt: "2020-02-28T14:38:32.000Z",
-          updatedAt: "2020-07-13T11:32:24.000Z",
-        },
-      },
-      {
-        id: 60,
-        text: "Officiis quis blanditiis magni et.",
-        UserId: 3,
-        RestaurantId: 10,
-        createdAt: "2020-02-28T14:38:32.000Z",
-        updatedAt: "2020-02-28T14:38:32.000Z",
-        User: {
-          id: 3,
-          name: "BB Bear",
-          email: "user2@example.com",
-          password:
-            "$2a$10$VHKmtPqbcUzK46qxLllqj.w506U2N2TObMmnpdlNG2CLZPa1xzuTi",
-          isAdmin: false,
-          image: "https://i.imgur.com/IaCnj2S.jpg",
-          createdAt: "2020-02-28T14:38:32.000Z",
-          updatedAt: "2020-08-11T13:08:12.000Z",
-        },
-      },
-      {
-        id: 4412,
-        text: "good!",
-        UserId: 1,
-        RestaurantId: 10,
-        createdAt: "2020-04-10T07:16:48.000Z",
-        updatedAt: "2020-04-10T07:16:48.000Z",
-        User: {
-          id: 1,
-          name: "root",
-          email: "root@example.com",
-          password:
-            "$2a$10$J9pLpJJ1Tzfe/ZcjdYwXdumyh.3F5E.w/HTxRcH./cl3azhgekgQe",
-          isAdmin: false,
-          image: "https://i.imgur.com/kTEFFYJ.png",
-          createdAt: "2020-02-28T14:38:32.000Z",
-          updatedAt: "2020-09-18T04:37:51.000Z",
-        },
-      },
-    ],
-  },
-};
+import restaurantsAPI from "../apis/restaurants.js";
+import { Toast } from "../utils/helpers.js";
 
 export default {
   name: "RestaurantDashboard",
@@ -109,11 +32,28 @@ export default {
     };
   },
   created() {
-    this.fetchRestaurant();
+    const restaurantId = this.$route.params.id;
+    this.fetchRestaurant(restaurantId);
   },
   methods: {
-    fetchRestaurant() {
-      this.restaurant = { ...dummyData.restaurant };
+    async fetchRestaurant(restaurantId) {
+      try {
+        const { data, statusText } = await restaurantsAPI.getRestaurant({
+          restaurantId,
+        });
+
+        if (statusText !== "OK") {
+          throw new Error();
+        }
+
+        this.restaurant = data.restaurant;
+      } catch (error) {
+        console.log("error", error);
+        Toast.fire({
+          icon: "error",
+          title: "無法顯示餐廳的資料",
+        });
+      }
     },
   },
 };
