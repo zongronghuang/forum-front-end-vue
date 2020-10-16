1fa242b08147379b887497f067d0ac98f8c5b830
Zong-Rong Huang <zongrong.h886@gmail.com>
2020-10-16 23:47:39 +0800

    chore: add command line for GitHub deployment via HTTPS in /deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,12 @@ git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-git push -f git@github.com:zongronghuang/forum-front-end-vue.git master:gh-pages
+
+# Deploy your app via SSH
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+
+# Deploy your app via HTTPS
+# git push -f https://github.com/<USERNAME>/<REPO>.git master:gh-pages
+git push -f https://github.com/zongronghuang/forum-front-end-vue.git master:gh-pages
 
 cd -
